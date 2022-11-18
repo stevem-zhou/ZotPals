467d66c22ffa16950d96b988905970997c1e9be0
Katy Huang <Katyh1@uci.edu>
2022-11-17 17:22:04 -0800

    Change Profile to round

--- client/src/Components/aboutComponents/Card.js
+++ client/src/Components/aboutComponents/Card.js
@@ -2,12 +2,13 @@ import React from "react";//use in cardlist
 
 const Card = ({ id, name, email, img }) => {
   return (
-    <div className="tc bg-light-yellow dib br3 pa4 ma4 hover shadow-5">
+    <div className="tc dib br3 pa4 ma4 hover">
       <img className="br-100 h5 w" alt="profile_pic" src={img}/>
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
       </div>
+    
     </div>
   );
 };
