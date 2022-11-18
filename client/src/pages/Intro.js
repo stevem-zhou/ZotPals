8da396f4a46dcc74e83e7c9786e39ae8e3255825
NwinNwin <ndnguyen2003@gmail.com>
2022-11-18 14:48:39 -0800

    added intro page

--- client/src/pages/Intro.js
+++ client/src/pages/Intro.js
@@ -1,32 +1,63 @@
 import React from "react";
+import "./Intro.css";
+import Carousel from "react-bootstrap/Carousel";
+import { Link } from "react-router-dom";
 
 export default function Intro() {
-    return ()}
-        <>
-        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img class="d-block w-100" src="https://media.discordapp.net/attachments/1036369852207738960/1043278554558902272/Untitled_Artwork_4.png?width=1046&height=1046" alt="First slide"/>
-          </div>
-          <div class="carousel-item">
-            <img class="d-block w-100" src="https://media.discordapp.net/attachments/1036369852207738960/1043279189991772321/Untitled_Artwork.png?width=1046&height=1046" alt="Second slide"/>
-          </div>
-          <div class="carousel-item">
-            <img class="d-block w-100" src="https://media.discordapp.net/attachments/1036369852207738960/1043279189991772321/Untitled_Artwork.png?width=1046&height=1046" alt="Third slide"/>
-          </div>
-        </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
+  return (
+    <div className="intro-div">
+      <div className="carousel-intro">
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className=" intro-img"
+              src="https://media.discordapp.net/attachments/1036369852207738960/1043278554558902272/Untitled_Artwork_4.png?width=1046&height=1046"
+              alt="First slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="intro-img"
+              src="https://media.discordapp.net/attachments/1036369852207738960/1043279189991772321/Untitled_Artwork.png?width=1046&height=1046"
+              alt="Second slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className=" intro-img"
+              src="https://media.discordapp.net/attachments/1036369852207738960/1043278554294652959/Untitled_Artwork_3.png?width=1046&height=1046"
+              alt="Third slide"
+            />
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <img
+              className=" intro-img"
+              src="https://media.discordapp.net/attachments/1036369852207738960/1043278554516955276/Untitled_Artwork_2.png?width=1046&height=1046"
+              alt="Fourth slide"
+            />
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <img
+              className=" intro-img"
+              src="https://media.discordapp.net/attachments/1036369852207738960/1043278554592456734/Untitled_Artwork_1.png?width=1046&height=1046"
+              alt="Fifth slide"
+            />
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <img
+              className=" intro-img"
+              src="https://media.discordapp.net/attachments/1036369852207738960/1043278554227556352/Untitled_Artwork.png?width=1046&height=1046"
+              alt="Sixth slide"
+            />
+          </Carousel.Item>
+        </Carousel>
       </div>
-      </>
+      <Link to="/home">
+        <button className="home-btn">❤️Home❤️</button>
+      </Link>
+    </div>
+  );
+}
